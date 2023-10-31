f792378af218e5523c459a555289398f5fa8f4fd
a10x <79578783+a10x@users.noreply.github.com>
2023-10-31 23:35:53 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { join, dirname } from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const token = "MTE2ODk3NjIwNTMzNzM0MjA0NQ.GxxdEE.TyZSCyoZSQwA3mBYMc0B-2tnkBGe5_JeaAu_2Q";
-export const guildId = "1058029898339467274";
+export const token = "token_here";
+export const guildId = "server_id";
 export const clientId = "1168976205337342045";
 export const saveFilePath = join(__dirname, "save", "save.json");
